baef8c776f02de06674a6d53646f948858727c35
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-03 10:22:49 -0500

    Add Politician Adapter function and code

---
+++ KnowYourGovernment/app/src/main/java/com/example/antoine/knowyourgovernment/PoliticianAdapter.java
@@ -0,0 +1,62 @@
+package com.example.antoine.knowyourgovernment;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by antoine on 4/3/17.
+ */
+
+public class PoliticianAdapter extends RecyclerView.Adapter<PoliticianListHolder>
+{
+    //Var TAG Declaration for log
+    private static final String TAG = "SummonerAdapter";
+
+    //Var List Declaration
+    private List<Politician> politicianList;
+
+    //Var MainActivity Declaration for onClickListener and onLongClickListener
+    private MainActivity mainAct;
+
+    public PoliticianAdapter(List<Politician> empList, MainActivity ma)
+    {
+        //Initializing stockList as an empty list
+        this.politicianList = empList;
+        //Initializing MainActivity Var
+        mainAct = ma;
+    }
+
+    @Override
+    public PoliticianListHolder onCreateViewHolder(final ViewGroup parent, int viewType)
+    {
+        Log.d(TAG, "onCreateViewHolder: MAKING NEW");
+        //Initializing the view holder to hold stock_list_row
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.government_row, parent, false);
+        //itemView.setOnClickListener(mainAct);
+        //itemView.setOnLongClickListener(mainAct);
+        return new PoliticianListHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(PoliticianListHolder holder, int position)
+    {
+        Log.d("Politician Adapter", "onBindViewHolder: BINDING");
+        Politician politician = politicianList.get(position);
+        holder.politicianFunction.setText(politician.getOffice());
+        Log.d("Politician Adapter", "onBindViewHolder: " + politician.getOffice());
+        holder.politicianName.setText("level: " + politician.getName());
+        Log.d("Politician Adapter", "onBindViewHolder: " + politician.getName());
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return politicianList.size();
+    }
+
+}
