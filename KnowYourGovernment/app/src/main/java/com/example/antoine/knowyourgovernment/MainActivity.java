b1bcf3dccecdd26c4001da11ee4fb8cf97c8ffec
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-04-03 09:28:06 -0500

    Add Ressource in String Add onCreateOPtionsMenu to link menu to MA

--- KnowYourGovernment/app/src/main/java/com/example/antoine/knowyourgovernment/MainActivity.java
+++ KnowYourGovernment/app/src/main/java/com/example/antoine/knowyourgovernment/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.antoine.knowyourgovernment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        //Adding Menu to MainActivity
+        Log.d(this.getString(R.string.TAGMA), "onCreateOptionsMenu");
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
 }
